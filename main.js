4ecc0133fd0c028f894e2eee9b5dcd653082b990
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-10 01:27:59 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,72 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded",function(){
+  document.getElementById("modal").className = "hidden"
+  
+  
+
+  document.addEventListener("click",function(e){
+      if (e.target.className === "like-glyph")
+      { activateHeart(e)}
+      else { dislikeHeart(e)}
+  })
+
+
+  function activateHeart(e){
+      mimicServerCall()
+      .then(() => { 
+        e.target.textContent = FULL_HEART
+        e.target.className = "activated-heart"
+      })
+      .catch((errors) => {
+        error.querySelector("h2").textContent = errors
+        error.className="";
+        setTimeout(function(){ error.className ="hidden"}, 5000)
+
+      
+      })
+
+    
+  }
+
+
+  function dislikeHeart(e){
+    mimicServerCall()
+    .then(() => { 
+      e.target.textContent = EMPTY_HEART
+      e.target.className = ""
+    })
+    .catch((errors) => {
+      error.querySelector("h2").textContent = errors
+      error.hidden=false;
+      setTimeout(function(){ error.hidden=true}, 5000)
+
+    
+    })
+
+  
+}
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
